5ecfa12609bc5a2adde8a94c129e8e82e400ba16
PraveenChoudhary <104825732+webframezs@users.noreply.github.com>
2025-01-07 05:52:08 +0000

    variables oprations

---
+++ .github/javascript/variables.js
@@ -0,0 +1,6 @@
+const userId = 1234; // constant variables not desclared and not assigned twice and block scope 
+let userScore = 423421; // let is a block scope variable and we can assign twice but no declaring 
+var data = 65236; // old method and global variables.
+
+userScore = 823847;
+console.log(userScore);
